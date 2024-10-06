1eb9939887a8a425ca161c27f861e43e3eecf761
rfmineguy <rfmineguy@gmail.com>
2024-10-06 15:21:30 -0700

    Arguments: Add descriptions to each command

--- arguments.go
+++ arguments.go
@@ -3,9 +3,9 @@ package main
 // #run methods implemented in commands.go
 
 type Register struct {
-	Name string `short:"n" long:"name" required:"true"`
-	Path string `short:"p" long:"path" required:"true"`
-	Tags string `short:"t" long:"tags" required:"true"`
+	Name string `short:"n" long:"name" required:"true" description:"The name of the project"`
+	Path string `short:"p" long:"path" required:"true" description:"The path of the project on your computer"`
+	Tags string `short:"t" long:"tags" required:"true" description:"The tags you wish to associate with your project"`
 	Enabled bool `hidden:"true" no-ini:"true"`
 }
 func (c *Register) Execute(args []string) error {
@@ -14,16 +14,15 @@ func (c *Register) Execute(args []string) error {
 }
 
 type List struct {
-	All bool `short:"a" long:"all" required:"false"`
-	Tags string `short:"t" long:"by-tags" required:"false"`
-	Name string `short:"n" long:"by-name" required:"false"`
+	All bool `short:"a" long:"all" required:"false" description:"Listing of all registered projects (can be long)"`
+	Tags string `short:"t" long:"by-tags" required:"false" description:"Listing of projects registered with the supplied tags"`
+	Name string `short:"n" long:"by-name" required:"false" description:"Listing of projects registered with the supplied name"`
 	Enabled bool `hidden:"true" no-ini:"true"`
 }
 func (c *List) Execute(args []string) error {
 	c.Enabled = true;
 	return nil;
 }
-
 
 var opts struct {
 	Register Register `command:"register"`
